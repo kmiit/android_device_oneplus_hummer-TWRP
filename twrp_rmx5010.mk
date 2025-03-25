3132617675404145354f76534463b3bcb242f2c7
kmiit <kmiit@foxmail.com>
2025-03-25 18:48:26 +0800

    rmx5010: init

---
+++ twrp_rmx5010.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/realme/rmx5010
+
+# Inherit from device.mk configuration
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := rmx5010
+
+## Device identifier
+PRODUCT_DEVICE := rmx5010
+PRODUCT_NAME := twrp_rmx5010
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX5010
+PRODUCT_MANUFACTURER := realme
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := $(PRODUCT_RELEASE_NAME)
+
+# Theme
+TW_STATUS_ICONS_ALIGN := center
+TW_Y_OFFSET := 111
+TW_H_OFFSET := -111
